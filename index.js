9fae2c29ced2c6a8d68ee3cfa97b919709b77d52
Arthur Carson <carson.bob.arthur@gmail.com>
2018-01-24 08:09:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+  
+}
+
+function nestedTarget() {
+  const thing = document.querySelector('#nested .target')
+  return thing
+}
+
+
+function deepestChild() {
+  const nodeOne = document.querySelector('div#grand-node')
+  const nodeTwo = nodeOne.querySelector('div')
+  const nodeThree = nodeTwo.querySelector('div')
+  const nodeFour = nodeThree.querySelector('div')
+  const nodeFive = nodeFour.querySelector('div')
+  return nodeFive
+  
+}
+
+
+function increaseRankBy(n) {
+  const whatever = document.getElementById('app').querySelectorAll('ul.ranked-list')
+  
+  for (let i = 0; i < whatever.length; i++) {
+    whatever[i].innerHTML = (i + n).toString()
+    }
+}
+
+
