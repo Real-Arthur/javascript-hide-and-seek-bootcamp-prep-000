e808b664be12c835728bd6b1b3837b2a60d75cdd
Arthur Carson <carson.bob.arthur@gmail.com>
2018-01-24 08:06:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function increaseRankBy(n) {
   const whatever = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   
   for (let i = 0; i < whatever.length; i++) {
-    whatever[i].innerHTML = i + n
+    whatever[i].innerHTML = (i + n)
     }
 }
 
